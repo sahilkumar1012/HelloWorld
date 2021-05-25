0cabec990a88935116315d4af79042ac8be31636
Chirag Garg <chirag.garg96@gmail.com>
2021-05-26 01:13:59 +0530

    Added Statement

--- src/main/java/com/example/helloworld/greedy/MeetingRoomII.java
+++ src/main/java/com/example/helloworld/greedy/MeetingRoomII.java
@@ -2,6 +2,28 @@ package com.example.helloworld.greedy;
 
 import java.util.Arrays;
 
+/**
+ *
+ * Leetcode 253. Meeting Rooms II
+ * Given an array of meeting time intervals intervals where intervals[i] = [starti, endi], return the minimum number of conference rooms required.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: intervals = [[0,30],[5,10],[15,20]]
+ * Output: 2
+ * Example 2:
+ *
+ * Input: intervals = [[7,10],[2,4]]
+ * Output: 1
+ *
+ *
+ * Constraints:
+ *
+ * 1 <= intervals.length <= 104
+ * 0 <= starti < endi <= 106
+ */
 public class MeetingRoomII {
 
     public int minMeetingRooms(int[][] intervals){
