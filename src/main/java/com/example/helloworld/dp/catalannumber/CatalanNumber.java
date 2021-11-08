58a72d7fedb1ffa6a45fccb91dd3c86671a55c85
Sahil <sahilkumar1012@gmail.com>
2021-11-08 14:59:48 +0530

    Junit added and CatalanNumberTest.java for sample test

---
+++ src/main/java/com/example/helloworld/dp/catalannumber/CatalanNumber.java
@@ -0,0 +1,33 @@
+package com.example.helloworld.dp.catalannumber;
+
+/**
+ * find nth catalan number
+ * resource : https://youtu.be/qqcN4ROOusM
+ */
+public class CatalanNumber {
+
+    public static int findNthCatalanNumber(int n){
+        int[] dp = new int[n+1];
+        dp[0] = 1;
+        dp[1] = 1;
+
+        // dp[i] means ith catalan number
+        for(int i=2; i<=n; ++i){
+            int l = 0;
+            int r = i-1;
+
+            while(l <= i-1){
+                dp[i] += dp[l] * dp[r];
+                l++;
+                r--;
+            }
+        }
+
+        return dp[n];
+    }
+
+//    public static void main(String[] args) {
+//        System.out.println(CatalanNumber.findNthCatalanNumber(3));
+//    }
+
+}
