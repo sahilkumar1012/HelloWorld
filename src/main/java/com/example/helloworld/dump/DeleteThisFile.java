eba19da72ba0de7972e006e0b949da1b2a337b4c
Sahil <sahilpcs@amazon.com>
2023-03-01 23:22:00 +0530

    setting up repo on amazon mac

---
+++ src/main/java/com/example/helloworld/dump/DeleteThisFile.java
@@ -0,0 +1,7 @@
+package com.example.helloworld.dump;
+
+public class DeleteThisFile {
+    public static void main(String[] args) {
+        System.out.println("hello world repo for practice. !");
+    }
+}
