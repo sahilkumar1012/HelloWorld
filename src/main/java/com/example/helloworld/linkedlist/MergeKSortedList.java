a5e52bce5176e774c58cbbc0e8348b873e22c33b
Sahil <sahilkumar1012@gmail.com>
2023-10-25 02:03:32 +0530

    Solution for 'Merge 'k' Sorted Lists' problem. leetcode 23

--- src/main/java/com/example/helloworld/linkedlist/MergeKSortedList.java
+++ src/main/java/com/example/helloworld/linkedlist/MergeKSortedList.java
@@ -2,11 +2,12 @@ package com.example.helloworld.linkedlist;
 
 import com.example.helloworld.linkedlist.model.ListNode;
 
+import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.PriorityQueue;
 
 /**
- *
+ * hard
  * leetcode 23. Merge k Sorted Lists
  * You are given an array of k linked-lists lists, each linked-list is sorted in ascending order.
  *
@@ -47,25 +48,102 @@ import java.util.PriorityQueue;
  */
 public class MergeKSortedList {
 
+    // Implementation, using heap
     public ListNode mergeKLists(ListNode[] lists) {
-        if(lists.length==0){
+        if (lists.length == 0) {
             return null;
         }
-        PriorityQueue<ListNode> pq = new PriorityQueue<ListNode>((o1, o2) -> o1.val - o2.val);
-        for(int i=0; i<lists.length; ++i){
-            if(lists[i]!=null) // handle nulls properly.
-                pq.offer(lists[i]);
+
+        PriorityQueue<ListNode> pq = new PriorityQueue<>(Comparator.comparingInt(o -> o.val));
+        for (ListNode listNode : lists) {
+            if (listNode != null) // handle nulls properly.
+                pq.offer(listNode);
         }
 
-        ListNode temp;
         ListNode list = new ListNode(-1), last = list;
-        while(!pq.isEmpty()){
-            temp = pq.poll();
-            last.next = temp;
-            last = last.next;
-            if(temp.next!=null) pq.offer(temp.next);
+        while (!pq.isEmpty()) {
+            ListNode node = pq.poll();
+            last.next = node; last = last.next;
+            if (node.next != null)
+                pq.offer(node.next);
         }
 
         return list.next;
+    }
+
+    // write a main method to test this function
+    public static void main(String[] args) {
+        MergeKSortedList mergeKSortedList = new MergeKSortedList();
+        ListNode[] lists = new ListNode[3];
+        lists[0] = ListNode.fromArray(new int[]{1, 4, 5});
+        lists[1] = ListNode.fromArray(new int[]{1, 3, 4});
+        lists[2] = ListNode.fromArray(new int[]{2, 6});
+        ListNode list = mergeKSortedList.mergeKLists(lists);
+        ListNode.print(list);
+    }
+
+}
+
+class MergeKSortedListDivideNConquer {
+    // Merging K sorted linked list using Divide and conquer technique
+    // We are not using any extra space O(1) space here and time complexity is O(nlogk)
+    public ListNode mergeKLists(ListNode[] lists) {
+        if (lists == null || lists.length == 0) {
+            return null;
+        }
+        return helper(lists, 0, lists.length - 1);
+    }
+
+    private ListNode helper(ListNode[] lists, int low, int high) {
+        if (low > high) {
+            return null;
+        } else if (low == high) {
+            return lists[low];
+        } else {
+            int mid = low + (high - low) / 2;
+            ListNode left = helper(lists, low, mid);
+            ListNode right = helper(lists, mid + 1, high);
+            return merge(left, right);
+        }
+    }
+
+
+    private ListNode merge(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        }
+        if (l2 == null) {
+            return l1;
+        }
+
+        ListNode dummy = new ListNode(-1);
+        ListNode t = dummy;
+        ListNode t1 = l1;
+        ListNode t2 = l2;
+        while (t1 != null && t2 != null) {
+            if (t1.val < t2.val) {
+                t.next = t1;
+                t = t.next;
+                t1 = t1.next;
+            } else {
+                t.next = t2;
+                t = t.next;
+                t2 = t2.next;
+            }
+        }
+
+        while (t1 != null) {
+            t.next = t1;
+            t = t.next;
+            t1 = t1.next;
+        }
+
+        while (t2 != null) {
+            t.next = t2;
+            t = t.next;
+            t2 = t2.next;
+
+        }
+        return dummy.next;
     }
 }
--- src/main/java/com/example/helloworld/linkedlist/model/ListNode.java
+++ src/main/java/com/example/helloworld/linkedlist/model/ListNode.java
@@ -5,8 +5,7 @@ public class ListNode {
     public int val;
     public ListNode next;
 
-    public ListNode() {
-    }
+    public ListNode() {}
 
     public ListNode(int val) {
         this.val = val;
@@ -15,6 +14,27 @@ public class ListNode {
     public ListNode(int val, ListNode next) {
         this.val = val;
         this.next = next;
+    }
+
+    // create fromArray
+    public static ListNode fromArray(int[] arr) {
+        ListNode head = new ListNode();
+        ListNode current = head;
+        for (int j : arr) {
+            current.next = new ListNode(j);
+            current = current.next;
+        }
+        return head.next;
+    }
+
+    // create print function
+    public static void print(ListNode head) {
+        ListNode current = head;
+        while (current != null) {
+            System.out.print(current.val + " ");
+            current = current.next;
+        }
+        System.out.println();
     }
 
 }
