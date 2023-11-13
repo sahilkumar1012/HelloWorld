6d4bb1279c9e1fedf74f49ead17b6b63cdac1d32
Sahil <sahilkumar1012@gmail.com>
2023-11-13 15:48:46 +0530

    leetcode 2785. Sort Vowels in a String

--- src/main/java/com/example/helloworld/string/others/SortVowelsinaString.java
+++ src/main/java/com/example/helloworld/string/others/SortVowelsinaString.java
@@ -40,7 +40,7 @@ import java.util.PriorityQueue;
  * s consists only of letters of the English alphabet in uppercase and lowercase.
  */
 public class SortVowelsinaString {
-    public String sortVowels(String s) {
+    public String sortVowelsPQ(String s) {
         PriorityQueue<Character> pq = new PriorityQueue<>( Comparator.comparingInt(ch -> (int)ch ) );
         StringBuilder sb = new StringBuilder();
 
@@ -64,6 +64,30 @@ public class SortVowelsinaString {
     boolean isVowel( Character c ){
         return c == 'a' || c == 'e' || c == 'o'|| c == 'u'|| c == 'i'
                 || c == 'A' || c == 'E' || c == 'O'|| c == 'U'|| c == 'I';
+    }
+
+    public String sortVowels(String s) {
+        int[] hash = new int[257];
+
+        for(char ch : s.toCharArray()){
+            if(isVowel(ch))
+                hash[ch]++;
+        }
+
+        int j = 0;
+        StringBuilder sb = new StringBuilder();
+        for(char ch : s.toCharArray()){
+            if(isVowel(ch)){
+                while(hash[j] == 0) j++;
+                sb.append((char)j);
+                hash[j]--;
+            }else{
+                sb.append(ch);
+            }
+
+        }
+
+        return sb.toString();
     }
 
 }
