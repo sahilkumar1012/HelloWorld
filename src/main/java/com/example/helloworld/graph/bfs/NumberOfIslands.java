274c9f9dd9523b968fd4ea49861b8bde5a9501d6
Sahil <sahilkumar1012@gmail.com>
2024-04-19 11:44:52 +0530

    LeetCode Daily

--- src/main/java/com/example/helloworld/graph/bfs/NumberOfIslands.java
+++ src/main/java/com/example/helloworld/graph/bfs/NumberOfIslands.java
@@ -5,6 +5,8 @@ package com.example.helloworld.graph.bfs;
  * leetcode 200. Number of Islands
  * problem link : https://leetcode.com/problems/number-of-islands/description/
  *
+ * code harmony reference video: https://youtu.be/3yZFYBNQU7A
+ *
  * Given an m x n 2D binary grid grid which represents a map of '1's (land) and '0's (water), return the number of islands.
  *
  * An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
@@ -51,7 +53,6 @@ public class NumberOfIslands {
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
                 if(grid[i][j] == '1'){
-                    // System.out.println(i + " " + j);
                     dfs(grid, i, j);    // consider this island.
                     ans++;
                 }
